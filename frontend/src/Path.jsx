640348c7dae7ca87ef6aefa9ea8c17f38df70522
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-07 00:53:45 -0800

    use of chgTheta to re-calc entire second tree & change viewBox

--- frontend/src/Path.jsx
+++ frontend/src/Path.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Branch from './Branch'
 import Vector from './Vectors'
 
@@ -14,54 +13,65 @@ const branchOrigin = new Branch(
   vector.components[0],
   vector.components[1]
 )
-const tree = []
-tree[0] = branchOrigin
+let tree = [branchOrigin]
+let secTree = [branchOrigin]
 
-const Path = ({ count, theta, colour }) => {
-  let j, i
-  for (j = 0; j < count; j++) {
-    strokeW *= SWdecr
-    for (i = tree.length - 1; i >= 0; i--) {
-      if (!tree[i].finished) {
-        tree.push(tree[i].branch(theta))
-        tree.push(tree[i].branch(-theta))
+const Path = ({ count, theta, chgTheta, colour }) => {
+  let j, i, tmpTree
+  if (!chgTheta) {
+    for (j = 0; j < count; j++) {
+      strokeW *= SWdecr
+      for (i = tree.length - 1; i >= 0; i--) {
+        if (!tree[i].finished) {
+          tree.push(tree[i].branch(theta))
+          tree.push(tree[i].branch(-theta))
+        }
+        tree[i].finished = true
+        strokeArray.push(strokeW, strokeW)
       }
-      tree[i].finished = true
-      strokeArray.push(strokeW, strokeW)
+    }
+  } else {
+    secTree = [branchOrigin]
+    if (count > 0) {
+      for (i = 1; i <= Math.ceil((tree.length - 1) / 2); i++) {
+        tmpTree = secTree[i - 1]
+        secTree.push(tmpTree.branch(theta))
+        secTree.push(tmpTree.branch(-theta))
+      }
     }
   }
-  // console.log(tree)
+  console.log('tree')
+  console.log(tree)
+  console.log('secTree')
+  console.log(secTree)
   // console.log(strokeArray)
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       xmlnsXlink='http://www.w3.org/1999/xlink'
       width='100%'
-      viewBox='-200 -500 400 600'
+      viewBox='-200 -200 400 300'
     >
-      {tree &&
-        tree.map((br, i) => {
-          const path = br.path()
-          const x1 = path[0]
-          const y1 = path[1]
-          const x2 = path[2]
-          const y2 = path[3]
-          const id = `path${i}`
-          return (
-            <path
-              key={id}
-              id={id}
-              fill='none'
-              stroke={colour}
-              strokeWidth={eval(strokeArray[i])}
-              d={
-                i === 0
-                  ? `M ${x1} ${y1} v -${y1}`
-                  : `m ${x1} ${y1} L ${x2} ${y2}`
-              }
-            />
-          )
-        })}
+      {(chgTheta ? secTree : tree).map((br, i) => {
+        const path = br.path()
+        const x1 = path[0]
+        const y1 = path[1]
+        const x2 = path[2]
+        const y2 = path[3]
+        const id = `path${i}`
+        return (
+          <path
+            key={id}
+            id={id}
+            fill='none'
+            stroke={colour}
+            strokeWidth={strokeArray[i]}
+            d={
+              i === 0 ? `M ${x1} ${y1} v -${y1}` : `m ${x1} ${y1} L ${x2} ${y2}`
+            }
+          />
+        )
+      })}
     </svg>
   )
 }
