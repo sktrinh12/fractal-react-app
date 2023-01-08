43fe646457e259069ab27e6d5120c6de4adc7b14
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-08 15:54:54 -0800

    use of type prop

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App slider={true} />} />
-      <Route path='triangle' element={<App slider={false} />} />
+      <Route path='/' element={<App type={'tree'} />} />
+      <Route path='triangle' element={<App type={'triangle'} />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   </BrowserRouter>
