004a986dc2b03854cf01e92cabdb5bebd7f8edd4
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-08 00:25:22 -0800

    pass slider prop to App to conditionally render from parent 'main.jsx'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,38 @@ import ColourPicker from './ColourPicker'
 import './App.css'
 import Path from './Path'
 import ThetaSlider from './Slider'
+import FractalTriangle from './FractalTriangle'
 
-function App() {
+function App({ slider }) {
   const startTheta = (7 * Math.PI) / 6
   const [count, setCount] = useState(0)
-  const [theta, setTheta] = useState(startTheta)
   const [colour, setColour] = useState('#30B05D') // greenish
   const [displayColourPicker, setDisplayColour] = useState(false)
+  const [theta, setTheta] = useState(startTheta)
+
   const handleCountClick = () => {
     console.log(count)
     setCount(count + 1)
   }
-  const updateSlider = (e, theta) => {
-    setTheta(theta)
-  }
   const handleResetCount = () => {
     setCount(0)
+  }
+  const updateSlider = (e, theta) => {
+    setTheta(theta)
   }
   return (
     <>
       {' '}
-      <Path count={count} theta={theta} colour={colour} />
-      <h1>Fractal Tree</h1>
-      <ThetaSlider theta={theta} updateSlider={updateSlider} colour={colour} />
+      {slider && <Path count={count} theta={theta} colour={colour} />}
+      {!slider && <FractalTriangle count={count} colour={colour} />}
+      <h1>Fractal {slider ? 'Tree' : 'Triangle'}</h1>
+      {slider && (
+        <ThetaSlider
+          theta={theta}
+          updateSlider={updateSlider}
+          colour={colour}
+        />
+      )}
       <ColourPicker
         colour={colour}
         setColour={setColour}
