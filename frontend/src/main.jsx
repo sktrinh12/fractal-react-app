a00ba2bf1aebdf92217577d42319cf7a94361413
spencer.trinh <spencer.trinh@kinnate.com>
2023-01-08 00:25:48 -0800

    use of react-dom router and re-use App compt

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NotFound from './NotFound'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App slider={true} />} />
+      <Route path='triangle' element={<App slider={false} />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+)
